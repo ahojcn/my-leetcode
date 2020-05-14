dbd16a539795f8e8dfd89413fa33efdb0fc1276c
ahojcn <ahojcn@gmail.com>
2020-05-14 20:46:09 +0800

    2020-05-14 20:46:09

---
+++ 剑指offer/面试题12-矩阵中的路径.cpp
@@ -0,0 +1,30 @@
+// https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
+
+// dfs + 回溯 + 剪枝
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[i].size(); ++j) {
+                if (dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int k) {
+        if (i >= board.size() || i < 0 || j >= board[0].size() || j < 0
+        || board[i][j] != word[k]) return false;
+        if (k == word.size() - 1) return true;
+
+        char tmp = board[i][j];
+        board[i][j] = '/';
+        bool res = dfs(board, word, i + 1, j, k + 1)
+            || dfs(board, word, i - 1, j, k + 1)
+            || dfs(board, word, i, j + 1, k + 1)
+            || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+    }
+};
