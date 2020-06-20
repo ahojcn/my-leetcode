bb884f96ad69c04336ba101b89caa0b62f3e5d9d
ahojcn <ahojcn@gmail.com>
2020-06-20 20:37:33 +0800

    2020-06-20 20:37:33

---
+++ nowcoder/剑指offer/从尾到头打印链表.cc
@@ -0,0 +1,23 @@
+// https://www.nowcoder.com/practice/d0267f7f55b3412ba93bd35cfa8e8035?tpId=13&&tqId=11156&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+
+/**
+*  struct ListNode {
+*        int val;
+*        struct ListNode *next;
+*        ListNode(int x) :
+*              val(x), next(NULL) {
+*        }
+*  };
+*/
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> arr;
+        while (head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+        vector<int> a(arr.rbegin(), arr.rend());
+        return a;
+    }
+};
